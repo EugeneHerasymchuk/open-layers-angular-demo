6812f2834ac1f9ada5454df64e658d01a104aa7b
Yevhenii Herasymchuk <evhenii.herasymchuk@gmail.com>
2021-12-17 11:27:01 +0100

    styling and api url

--- src/app/modules/config/config.module.ts
+++ src/app/modules/config/config.module.ts
@@ -2,10 +2,12 @@ import { InjectionToken, NgModule } from "@angular/core"
 
 export interface AppConfig {
   subscriptionKey: string
+  api: string
 }
 export const APP_CONFIG = new InjectionToken<AppConfig>('app config')
 const APP_PROD_CONFIG: AppConfig = {
-  subscriptionKey: 'ImRQfoC5_nPc0SBcsdzKCj_j9-5NqmKmEgAMa8PbnAQ'
+  subscriptionKey: 'ImRQfoC5_nPc0SBcsdzKCj_j9-5NqmKmEgAMa8PbnAQ',
+  api: 'https://summitdays2021-gigalocation.azurewebsites.net/api/v1/kids/'
 }
 
 @NgModule({
