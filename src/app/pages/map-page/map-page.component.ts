6810da24b5ff2769775cb04e0e410a719b34a480
Yevhenii Herasymchuk <evhenii.herasymchuk@gmail.com>
2021-12-17 12:35:24 +0100

    Add random coordinates

--- src/app/pages/map-page/map-page.component.ts
+++ src/app/pages/map-page/map-page.component.ts
@@ -9,10 +9,10 @@ import { GiftService } from 'src/app/services/gift/gift.service';
   styleUrls: ['./map-page.component.sass']
 })
 export class MapPageComponent implements OnInit {
-  gifts$: Observable<Gift[]>;
+  // gifts$: Observable<Gift[]>;
 
   constructor(private giftService: GiftService) {
-    this.gifts$ = this.giftService.getChildrenGifts();
+    // this.gifts$ = this.giftService.getChildrenGifts();
   }
 
   ngOnInit(): void {
